0c5c6f02de9dd71f081bc0532c509973a03b23dc
Daniel Andrews <danielandrewsre@gmail.com>
2017-09-25 00:08:50 -0700

    working orangeoranges

---
+++ Assets/GameScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+	private int lives;
+	private int points;
+	private GameObject[] slots;
+
+	// Use this for initialization
+	void Start () {
+		lives = 3;
+		points = 0;
+		slots = GameObject.FindGameObjectsWithTag("TapSlot");
+		InvokeRepeating ("setToPeeled", 1, 1);
+	}
+
+	void setToPeeled () {
+		int index = Random.Range (0, slots.Length);
+		while (!slots[index].GetComponent<SlotScript>().ChangeStateToPeeled()) {
+			index = Random.Range (0, slots.Length);
+		}
+	}
+		
+
+	public void LoseLife() {
+		lives -= 1;
+		//update lives text
+		print(lives);
+		if (lives == 0) {
+			EndGame ();
+		}
+	}
+
+	private void EndGame() {
+		//end the game and show Game Over text
+	}
+
+	public void AddPoints() {
+		points += 100;
+		print (points);
+		//update points text;
+	}
+}
---
+++ Assets/SlotScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotScript : MonoBehaviour {
+
+	private int state;
+	private SpriteRenderer sr;
+	public GameObject gm;
+	private GameScript gs;
+	public Sprite peeled;
+	public Sprite unpeeled;
+
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = -1;
+		gm = GameObject.Find ("GameManager");
+		gs = gm.GetComponent<GameScript> ();
+		state = 0;
+		sr = GetComponentInChildren<SpriteRenderer> ();
+	}
+	
+	public bool ChangeStateToPeeled() {
+		if (state == 1) {
+			return false;
+		}
+
+		startTime = Time.time;
+		state = 1;
+		sr.sprite = peeled;
+
+		return true;
+	}
+
+	void ChangeStateToUnpeeled() {
+		sr.sprite = unpeeled;
+		state = 0;
+		startTime = -1;
+	}
+
+	void Update() {
+		if (startTime > -1) {
+			startTime -= Time.deltaTime;
+			if (startTime < 0) {
+				gs.LoseLife ();
+				ChangeStateToUnpeeled ();
+			}
+		}
+	}
+
+	void OnMouseEnter() {
+		if(Input.GetMouseButtonDown(0)){
+			if (state == 0) {
+				gs.LoseLife ();
+			} else if (state == 1) {
+				gs.AddPoints ();
+				ChangeStateToUnpeeled ();
+			}
+		}
+	}
+}
